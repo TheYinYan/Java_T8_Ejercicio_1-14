9a651d0bb622fe54ac2a12aaf8c47641e6020280
Samuel <samuelruizmartin2412@gmail.com>
2025-11-23 14:00:51 +0100

    Hecho hatas quitaPorDetras

--- src/App.java
+++ src/App.java
@@ -1,139 +1,79 @@
 public class App {
-    /**
-     * Devuelve verdadero si el número que se pasa como parámetro es capicúa
-     * y falso en caso contrario.
-     * 
-     * @param num --> Numero
-     * @return True o False
-     */
-    public static boolean esCapicua(int num) {
-        boolean capicua = false;
-        if (num == voltear(num)) {
-            capicua = true;
-        }
-        return capicua;
-    }
-
-    /**
-     * Devuelve el menor primo que es mayor al número que se pasa como parámetro.
-     * 
-     * @param num --> Numero
-     * @return El siguente primo
-     */
-    public static int siguientePrimo(int num) {
-        int nextPrimo = num + 1;
-        while (esPrimo(nextPrimo) == false) {
-            nextPrimo++;
-        }
-        return nextPrimo;
-    }
-
-    /**
-     * Devuelve verdadero si el número que se pasa como parámetro es primo y falso
-     * en caso contrario
-     * 
-     * @param num --> Numero
-     * @return True o False
-     */
-    public static boolean esPrimo(int num) {
-        boolean primo = true;
-        if (num == 1) {
-            primo = false;
-        }
-        for (int i = 2; i < num; i++) {
-            if ((double) (num % i) == 0) {
-                primo = false;
-            }
-        }
-        return primo;
-    }
-
-    /**
-     * Le da la vuelta a un número
-     * 
-     * @param num --> Numero
-     * @return Numero volteado
-     */
-    public static int voltear(int num) {
-        int voltear = 0;
-        int digitos = digitos(num);
-        while (digitos > 0) {
-            int digito = num % 10;
-            voltear = voltear * 10 + digito;
-            num /= 10;
-            digitos--;
-        }
-        return voltear;
-    }
-
-    /**
-     * Cuenta el número de dígitos de un número entero.
-     * 
-     * @param num --> Numero
-     * @return Numero de digitos
-     */
-    public static int digitos(int num) {
-        num = num * 10 + 1;
-        int digitos = 0;
-        while (num > 0) {
-            digitos++;
-            num /= 10;
-        }
-        digitos--;
-        return digitos;
-    }
-
     public static void main(String[] args) throws Exception {
         boolean salir = false;
         do {
             System.out.println("""
 
-                    0. Salir
-                    1. digitos
-                    2. Voltea
-                    3. esCapicua
-                    4. esPrimo
-                    5. siguientePrimo
-                    6. 
+                    Elige una opción
+
+                    0.  Salir
+                    1.  Digitos
+                    2.  Voltea
+                    3.  esCapicua
+                    4.  esPrimo
+                    5.  siguientePrimo
+                    6.  Potencia
+                    7.  digitoN
+                    8.  posicionDeDigito
+                    9.  quitaPorDetras
+                    10. quitaPorDelante
+                    11.
+                    12.
+                    13.
+                    14.
                     """);
             int opcion = Integer.parseInt(System.console().readLine("Opción "));
-            int num = Integer.parseInt(System.console().readLine("Introduzca numero "));
+            int num = 0;
+            int num2 = 0;
+
+            if (opcion > 0 && opcion < 14)
+                num = Integer.parseInt(System.console().readLine("Introduzca numero "));
+            if (opcion == 6 || opcion == 7 || opcion == 8 || opcion == 9)
+                num2 = Integer.parseInt(System.console().readLine("Introduzca otro numero "));
 
             switch (opcion) {
                 case 0:
                     salir = true;
+                    Mates.limpiarPantalla();
                     break;
 
                 case 1:
-                    System.out.printf("El numero %d tiene %d dígito/s%n", num, digitos(num));
+                    System.out.printf("El numero %d tiene %d dígito/s%n", num, Mates.digitos(num));
                     break;
 
                 case 2:
-                    System.out.printf("El %d volteado es %d%n", num, voltear(num));
+                    System.out.printf("El %d volteado es %d%n", num, Mates.voltear(num));
                     break;
 
                 case 3:
-                    System.out.printf("El %d%s es capicua%n", num, ((esCapicua(num)) ? "" : " no"));
+                    System.out.printf("El %d%s es capicua%n", num, ((Mates.esCapicua(num)) ? "" : " no"));
                     break;
 
                 case 4:
-                    System.out.printf("El %d%s es primo%n", num, ((esPrimo(num)) ? "" : " no"));
+                    System.out.printf("El %d%s es primo%n", num, ((Mates.esPrimo(num)) ? "" : " no"));
                     break;
 
                 case 5:
-                    System.out.printf("El siguiente primo mayor a %d es %d%n", num, siguientePrimo(num));
+                    System.out.printf("El siguiente primo mayor a %d es %d%n", num, Mates.siguientePrimo(num));
                     break;
 
                 case 6:
+                    System.out.printf("%d^%d = %d%n", num, num2, Mates.potencia(num, num2));
                     break;
 
                 case 7:
+                    System.out.printf("En la posición %d del numero %d esta el %d%n", num2, num,
+                            Mates.digitoN(num, num2));
                     break;
 
                 case 8:
+                    System.out.printf("En el %d, el dígito %d está en la posición %d", num, num2,
+                            Mates.posicionDeDigito(num, num2));
                     break;
 
                 case 9:
+                    System.out.printf("Si el %d, se le quita por detras %d dígito , se queda como %d%n", num, num2,
+                            Mates.quitaPorDetras(num, num2));
                     break;
 
                 case 10:
@@ -150,10 +90,11 @@ public class App {
 
                 case 14:
                     break;
-                
+
                 default:
-                    System.out.println("Introduce una opcion correcta");
+                    System.out.println("Introduce una opción correcta");
                     break;
+
             }
         } while (!salir);
 
---
+++ src/Mates.java
@@ -0,0 +1,168 @@
+public class Mates {
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    /**
+     * Dada una base y un exponente devuelve la potencia.
+     * 
+     * @param base      Base de la potencia
+     * @param exponente Exponente de la potencia
+     * @return Potencia
+     */
+    public static int potencia(int base, int exponente) {
+        int num = base;
+        for (int i = 1; i <= exponente; i++) {
+            if (i == 1)
+                num = base;
+            else
+                num = num * base;
+        }
+        return num;
+    }
+
+    /**
+     * Devuelve verdadero si el número que se pasa como parámetro es capicúa
+     * y falso en caso contrario.
+     * 
+     * @param num Numero
+     * @return True o False
+     */
+    public static boolean esCapicua(int num) {
+        boolean capicua = false;
+        if (num == voltear(num))
+            capicua = true;
+        return capicua;
+    }
+
+    /**
+     * Devuelve el menor primo que es mayor al número que se pasa como parámetro.
+     * 
+     * @param num Numero
+     * @return El siguente primo
+     */
+    public static int siguientePrimo(int num) {
+        int nextPrimo = num + 1;
+        while (esPrimo(nextPrimo) == false) {
+            nextPrimo++;
+        }
+        return nextPrimo;
+    }
+
+    /**
+     * Devuelve verdadero si el número que se pasa como parámetro es primo y falso
+     * en caso contrario
+     * 
+     * @param num Numero
+     * @return True o False
+     */
+    public static boolean esPrimo(int num) {
+        boolean primo = true;
+        if (num == 1)
+            primo = false;
+        for (int i = 2; i < num; i++) {
+            if ((double) (num % i) == 0)
+                primo = false;
+        }
+        return primo;
+    }
+
+    /**
+     * Le quita a un número n dígitos por detrás (por la derecha).
+     * 
+     * @param num Numero
+     * @param digitos a quitar
+     * @return numeros con los digitos quitados
+     */
+    public static int quitaPorDetras(int num, int digitos) {
+        int aux = 0;
+        int voteado = voltear(num);
+        for (int i = 0; i < digitos(num) - digitos; i++) {
+            int digito = voteado % 10;
+            aux = aux * 10 + digito;
+            voteado /= 10;
+        }
+        return aux;
+    }
+
+    /**
+     * Da la posición de la primera ocurrencia de un dígito dentro de un número
+     * entero.
+     * Si no se encuentra, devuelve -1.
+     * 
+     * @param num    Numero
+     * @param digito del numero
+     * @return Posición del digito o -1 si digito no esta en el numero
+     */
+    public static int posicionDeDigito(int num, int digito) {
+        int posicion = 1;
+        int voteado = voltear(num);
+        boolean correcto = false;
+        while (voteado > 0 && !correcto) {
+            int digitos = voteado % 10;
+            if (digitos == digito)
+                correcto = true;
+            else
+                posicion++;
+            voteado /= 10;
+        }
+        if (!correcto)
+            posicion = -1;
+        return posicion;
+    }
+
+    /**
+     * Devuelve el dígito que está en la posición n de un número entero.
+     * Se empieza contando por el 0 y de izquierda a derecha.
+     * 
+     * @param num      Numero
+     * @param posicion del numero
+     * @return Digito en la posicion del numero
+     */
+    public static int digitoN(int num, int posicion) {
+        int digito = 0;
+        int voteado = voltear(num);
+        for (int i = 0; i < digitos(num); i++) {
+            if (i == posicion)
+                digito = voteado % 10;
+            voteado /= 10;
+        }
+        return digito;
+    }
+
+    /**
+     * Le da la vuelta a un número
+     * 
+     * @param num Numero
+     * @return Numero volteado
+     */
+    public static int voltear(int num) {
+        int voltear = 0;
+        int digitos = digitos(num);
+        while (digitos > 0) {
+            int digito = num % 10;
+            voltear = voltear * 10 + digito;
+            num /= 10;
+            digitos--;
+        }
+        return voltear;
+    }
+
+    /**
+     * Cuenta el número de dígitos de un número entero.
+     * 
+     * @param num Numero
+     * @return Numero de digitos
+     */
+    public static int digitos(int num) {
+        num = num * 10 + 1;
+        int digitos = 0;
+        while (num > 0) {
+            digitos++;
+            num /= 10;
+        }
+        digitos--;
+        return digitos;
+    }
+}
